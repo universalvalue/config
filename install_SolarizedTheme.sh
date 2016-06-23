bf98d0b0a4cc185e17daecdfeff6d6c73ba0a799
Max <maximilian.mussotter@gmx.de>
2016-06-23 14:31:38 +0200

    update Solarized install script for more color in ls output

--- install_SolarizedTheme.sh
+++ install_SolarizedTheme.sh
@@ -1,2 +1,4 @@
 git clone https://github.com/Anthony25/gnome-terminal-colors-solarized ~/Projects/gnome_terminal_colors_solarized
 ~/Projects/gnome_terminal_colors_solarized/install.sh
+git clone https://github.com/seebi/dircolors-solarized#ansi ~/Projects/gnome_terminal_dircolors
+cp ~/Project/gnome_terminal_dircolors/dircolors.ansi-dark ~/.dircolors
