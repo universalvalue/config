30a7bd37125c8c861aac35f124c874727b2e47ef
Max <maximilian.mussotter@gmx.de>
2016-06-23 13:28:45 +0200

    add xmodmap.desktop to change key behaviour on start

--- install_symlinks.sh
+++ install_symlinks.sh
@@ -1,2 +1,3 @@
 rm ~/.vimrc; ln -s ~/Projects/config/.vimrc ~/.vimrc
 rm ~/.Xmodmap; ln -s ~/Projects/config/.Xmodmap ~/.Xmodmap
+rm ~/.bash_aliases; ln -s ~/Projects/config/.bash_aliases ~/.bash_aliases; source ~/.bashrc
