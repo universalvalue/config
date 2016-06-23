8d777e90326fbe026f07c3d06524b23f4a86f49e
Maximilian Mussotter <mussott@lfluids43.mpi-stuttgart.mpg.de>
2016-06-23 16:12:41 +0200

    updated Solarized_Theme;wrong path

--- install_SolarizedTheme.sh
+++ install_SolarizedTheme.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/Anthony25/gnome-terminal-colors-solarized ~/Projects/gnome_terminal_colors_solarized
 ~/Projects/gnome_terminal_colors_solarized/install.sh
 git clone https://github.com/seebi/dircolors-solarized ~/Projects/gnome_terminal_dircolors
-cp ~/Project/gnome_terminal_dircolors/dircolors.ansi-dark ~/.dircolors
+cp ~/Projects/gnome_terminal_dircolors/dircolors.ansi-dark ~/.dircolors
