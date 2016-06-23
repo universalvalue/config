9d1b474855d78cde684355ebd2af88f14dc2bc79
Max <maximilian.mussotter@gmx.de>
2016-06-23 13:56:06 +0200

    update install script for links; now includes .bashrc and .bash_aliases

--- install_symlinks.sh
+++ install_symlinks.sh
@@ -1,3 +1,4 @@
 rm ~/.vimrc; ln -s ~/Projects/config/.vimrc ~/.vimrc
 rm ~/.Xmodmap; ln -s ~/Projects/config/.Xmodmap ~/.Xmodmap
-rm ~/.bash_aliases; ln -s ~/Projects/config/.bash_aliases ~/.bash_aliases; source ~/.bashrc
+rm ~/.bash_aliases; ln -s ~/Projects/config/.bash_aliases ~/.bash_aliases
+rm ~/.bashrc; ln -s ~/Projects/config/.bashrc ~/.bashrc
